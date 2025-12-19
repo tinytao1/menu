3ca981c9df2b79d25baea96ee65bdd8eb5239ec5
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-12-19 14:26:18 -0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,18 @@
+function toggleDoubleSingleButton() {
+  let doubleButtonEl = document.getElementById('doubleButton');
+  if (doubleButtonEl.value == '2X order') {doubleButtonEl.value="1X order"}
+  else {doubleButtonEl.value="2X order"}
+}
+
+function myReload () { location.reload(); }  // reloads the current page from chache
+
+function myAlert5() {alert(5);}
+
+function toggleDoubleOrder() {
+  element.r1cb4.classList.toggle("double-order");
+  element.r1cb5.classList.toggle("double-order");
+  element.r1cb6.classList.toggle("double-order");
+  element.r2cb4.classList.toggle("double-order");
+  element.r2cb5.classList.toggle("double-order");
+  element.r2cb6.classList.toggle("double-order");
+}
---
+++ references.js
@@ -0,0 +1,24 @@
+// const doubleButtonEl = document.getElementById('doubleButton');
+
+const element = { 
+// DOM element references
+// element.r1cb4.style.visibility = "hidden"
+// document.getElementById('r1cb4').style.visibility = "hidden"
+// TEST:  const r1cb1 = document.getElementById('r1cb1');
+    r1cb1: document.getElementById('r1cb1'),
+    r1cb2: document.getElementById('r1cb2'),
+    r1cb3: document.getElementById('r1cb3'),
+    r1cb4: document.getElementById('r1cb4'),
+    r1cb5: document.getElementById('r1cb5'),
+    r1cb6: document.getElementById('r1cb6'),
+    
+    r2cb1: document.getElementById('r2cb1'),
+    r2cb2: document.getElementById('r2cb2'),
+    r2cb3: document.getElementById('r2cb3'),
+    r2cb4: document.getElementById('r2cb4'),
+    r2cb5: document.getElementById('r2cb5'),
+    r2cb6: document.getElementById('r2cb6'),
+}
+
+
+   
