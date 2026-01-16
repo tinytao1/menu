f44e1f092611a9158e47c0a4194f61554322cae5
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2026-01-15 16:51:13 -0800

    Implement date formatting and display function
    Added two date formatters and a function to format date over time.

--- functions/date-function.js
+++ functions/date-function.js
@@ -12,4 +12,24 @@ const formattedDate =
 new Intl.DateTimeFormat('en-US', options).format(date);
 // Example output for the time of this response: "December 20, 2025 at 5:42 PM"
 
+const ALSO_THIS;
+
+const date2 = new Date();
+
+const formatter1 = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    year: 'numeric',
+    month: 'long',
+});
+
+const formatter2 = new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true // Use 12-hour time with AM/PM
+});
+
+function dateOverTime() { 
+        return formatter1.format(date2) + "<br>" +
+               formatter2.format(date2) ; }
 
