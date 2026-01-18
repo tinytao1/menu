4ff1701316d5f9db885c57585ab83fcf2a0de3b6
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2026-01-17 17:18:11 -0800

    Add event listeners for reload and reset buttons

--- event-listn.js
+++ event-listn.js
@@ -1,4 +1,6 @@
 document.getElementById('reload').addEventListener('click', myReload); // location.reload); 
+
+document.getElementById('reset-with-confirmation').addEventListener('click', resetWithConfirmation); // 
   
 doubleButton.addEventListener
   ('click', doubleOrderClick); // location.reload); 
